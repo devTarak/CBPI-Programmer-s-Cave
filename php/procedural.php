5dceaf78633691331b8f112cf812cbbf715a6fbb
Tarak Rahman <tarakrahman421@gmail.com>
2025-04-23 02:34:53 +0600

    Create procedural.php

---
+++ php/procedural.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <p>hello Php>></p>
+        <?php
+        echo "Hello PHP2";
+        /* Hello This is
+        multiline comment
+        understand */
+        // Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nostrum porro facere tenetur aliquam voluptas dolor doloribus omnis voluptatum, minima placeat autem nisi obcaecati, ipsam ipsum modi odio. Exercitationem, architecto eligendi, veniam nostrum praesentium magnam quos quo natus voluptatum delectus eaque distinctio nulla. Accusantium, esse consectetur ut sint modi et sapiente nesciunt, omnis a aliquam quisquam exercitationem facilis alias quae hic soluta voluptatem deleniti iusto aspernatur nihil maxime nostrum! Perspiciatis quo fugit quibusdam! Eum deleniti iusto fuga ullam dolores non culpa sapiente. Sequi nisi numquam accusantium, commodi molestiae, maiores fugit aspernatur quia ipsa consequuntur magnam repellendus, suscipit placeat asperiores fugiat. Debitis alias officia, dolore vitae modi sequi maxime numquam 
+        // veritatis! Enim animi vitae, et architecto rerum officiis dignissimos, amet delectus expedita quisquam quaerat nisi atque voluptatibus. Eius illum odio quia quam, dolorem aliquid optio atque assumenda cumque sit dolorum laudantium dignissimos quod tenetur, quasi sequi! Aperiam quibusdam dolor doloremque nisi atque reprehenderit quis incidunt, soluta in voluptatum officiis quisquam laudantium odio. Ut libero aperiam maxime dolore eligendi natus nihil repellendus beatae fugit laboriosam harum consectetur quae, est eos corporis facere aspernatur eaque explicabo! Aperiam nisi est ea ab consequuntur, quasi, magnam id culpa ratione asperiores quis eos modi enim corporis harum facilis labore facere 
+        // laudantium ipsa perferendis! Laborum culpa sint ipsam possimus veritatis autem hic distinctio molestiae recusandae doloribus quasi velit nisi adipisci suscipit, ipsa obcaecati, at libero labore, voluptatibus quas? Iste minima doloribus asperiores suscipit officia quam in ducimus dolore fuga, et itaque ea omnis placeat error laborum adipisci. Sunt qui dicta consequatur commodi fugit corrupti provident inventore est? Magni atque placeat sed autem hic eius tempora natus, dolor ratione delectus qui ipsa praesentium. Quos consequatur cum iusto delectus commodi laborum nam dicta harum illum vel adipisci sit assumenda laboriosam, ad fugit error accusantium vitae fuga! Accusamus voluptatem illo aliquam quis aliquid itaque deserunt neque at doloremque. Atque soluta nulla voluptates itaque ad fuga accusantium sit ab totam incidunt, impedit quos!
+        $variable1 = 34;
+        $variable2 = 45;
+        echo $variable1;
+        echo $variable2;
+        echo $variable1 + $variable2;
+        // Operators in PHP
+        // Arithmetic Operators
+        $arvar1 = 10;
+        $arvar2 = 20;
+        $sum = $arvar1 + $arvar2;
+        echo "<br>Sum: " . $sum;
+        echo "<br>";
+        echo "substraction: ";
+        echo $arvar1 - $arvar2;
+        echo "<br>";
+        echo "Multipication: ";
+        echo $arvar1*$arvar2;
+        echo "<br>";
+        echo "Division: ";
+        echo $arvar1/$arvar2;
+        // Assignment Operators
+        $newVar = $arvar1;
+        echo "<br>";
+        echo "New variable: ";
+        echo $newVar;
+        echo "<br>";
+        $newVar += 5;
+        echo "new variable: ";
+        echo $newVar;
+        $newVar -=5;
+        echo "<br>";
+        echo "new variable: ";
+        echo $newVar;
+        // Comparison Operators
+        // Logical Operators
+        // Increment/Decrement Operators
+
+        ?>
+    </div>
+</body>
+</html>
